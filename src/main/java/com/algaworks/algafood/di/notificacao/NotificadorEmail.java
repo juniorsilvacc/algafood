dd29a018adbd7d6cedca7e9d048497cc2a497706
juniorsilvacc <juniorsilvacc@hotmail.com>
2022-11-19 00:59:00 -0300

    changing project configuration depending on environment

--- src/main/java/com/algaworks/algafood/di/notificacao/NotificadorEmail.java
+++ src/main/java/com/algaworks/algafood/di/notificacao/NotificadorEmail.java
@@ -1,21 +1,22 @@
 package com.algaworks.algafood.di.notificacao;
 
-import org.springframework.context.annotation.Profile;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.algaworks.algafood.di.modelo.Cliente;
 
-@Profile("prod")
 @TipoNotificador(NivelUrgencia.NORMAL)
 @Component
 public class NotificadorEmail implements Notificador {
 	
-	public NotificadorEmail() {
-		System.out.println("NotificadorEmail REAL");
-	}
+	@Autowired
+	private NotificadorProperties properties;
 	
 	@Override
 	public void notificar(Cliente cliente, String messagem) {
+		System.out.println("Host: " + properties.getHostServidor());
+		System.out.println("Port: " + properties.getPortaServidor());
+		
 		System.out.printf("Notificando %s através do e-mail %s - %s",
 				cliente.getNome(), cliente.getEmail(), messagem);
 	}
