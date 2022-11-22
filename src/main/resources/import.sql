cb19ddaeea07a2aebd0599bc0a5ad73c4f82d5d6
juniorsilvacc <juniorsilvacc@hotmail.com>
2022-11-22 18:00:28 -0300

    Desafio - Lombok e repositório de restaurantes

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,5 @@
 INSERT INTO cozinha (nome) VALUES ('Tailandesa');
 INSERT INTO cozinha (nome) VALUES ('Indiana');
+
+INSERT INTO restaurante (nome) VALUES ('Sertanejo');
+INSERT INTO restaurante (nome) VALUES ('China');
