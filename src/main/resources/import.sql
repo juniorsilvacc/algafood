78f4a6ad1e747a730f0bc03a7945f663aed31911
juniorsilvacc <juniorsilvacc@hotmail.com>
2022-11-22 18:52:17 -0300

    Mapeando relacionamento com @ManyToOne

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,7 @@
-INSERT INTO cozinha (nome) VALUES ('Tailandesa');
-INSERT INTO cozinha (nome) VALUES ('Indiana');
+INSERT INTO cozinha (id, nome) VALUES (1, 'Tailandesa');
+INSERT INTO cozinha (id, nome) VALUES (2, 'Chinesa');
+INSERT INTO cozinha (id, nome) VALUES (3, 'Arabe');
 
-INSERT INTO restaurante (nome) VALUES ('Sertanejo');
-INSERT INTO restaurante (nome) VALUES ('China');
+INSERT INTO restaurante (nome, taxa_frete, cozinha_id) VALUES ('Thai Delivery', 20, 1);
+INSERT INTO restaurante (nome, taxa_frete, cozinha_id) VALUES ('China Gourmet', 18, 2);
+INSERT INTO restaurante (nome, taxa_frete, cozinha_id) VALUES ('Tur', 12, 3);
