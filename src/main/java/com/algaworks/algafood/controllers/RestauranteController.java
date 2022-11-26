e6cabc1e0adb6340183de33539ef706249e9736e
juniorsilvacc <juniorsilvacc@hotmail.com>
2022-11-26 16:20:29 -0300

    Modelando e implementando a inclusão de recursos de restaurantes

--- src/main/java/com/algaworks/algafood/controllers/RestauranteController.java
+++ src/main/java/com/algaworks/algafood/controllers/RestauranteController.java
@@ -7,27 +7,48 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
 import com.algaworks.algafood.domain.model.Restaurante;
 import com.algaworks.algafood.domain.repository.RestauranteRepository;
+import com.algaworks.algafood.domain.service.CadastroRestauranteService;
 
 @RestController
 @RequestMapping(value = "/restaurantes")
 public class RestauranteController {
 
 	@Autowired
-	private RestauranteRepository repository;
+	private RestauranteRepository repositoryRestaurante;
+	
+	@Autowired
+	private CadastroRestauranteService cadastroRestauranteService;
+	
+	@PostMapping(value = "/criar")
+	public ResponseEntity<?> adicionar(@RequestBody Restaurante restaurante) {
+		try {
+			
+			Restaurante novoRestaurante = cadastroRestauranteService.salvar(restaurante);
+			
+			return ResponseEntity.status(HttpStatus.CREATED).body(novoRestaurante);
+			
+		} catch (EntidadeNaoEncontradaException e) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+		}
+		
+	}
 	
 	@GetMapping
 	public List<Restaurante> listar() {
-		return repository.listar();
+		return repositoryRestaurante.listar();
 	}
 	
-	@GetMapping(value = "/{id}")
+	@GetMapping(value = "/{id}") 
 	public ResponseEntity<Restaurante> buscar(@PathVariable Long id) {
-		Restaurante resturante = repository.buscar(id);
+		Restaurante resturante = repositoryRestaurante.buscar(id);
 		
 		if (resturante == null ) {
 			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
